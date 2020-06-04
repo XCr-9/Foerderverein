e83ed6139471c1540c771c0a6ad2dffe1b15adfb
XCr-9 <t.t.first.dev@gmail.com>
2020-06-04 02:14:20 +0200

    Add content structure

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
+
+import { Context } from '../../Context';
+import { Heading } from '../Headings';
 
 export default function Contact() {
+  const { content } = useContext(Context);
+
   return (
-    <Fragment>
-      <h1>Kontakt</h1>
-    </Fragment>
+    <div className="wrap">
+      <Heading h1 title={content.contact.heading} />
+
+      {content.contact.sections.map((section) => (
+        <Fragment key={section.heading}>
+          <Heading h2 title={section.heading} style={{ marginTop: '50px' }} />
+          {section.content.map((line) => (
+            <Fragment key={line.heading}>
+              {line}
+              <br />
+            </Fragment>
+          ))}
+        </Fragment>
+      ))}
+    </div>
   );
 }
