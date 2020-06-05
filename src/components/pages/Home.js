588d1fdd556f4b6bb1052d2dc44ea70fea338756
XCr-9 <t.t.first.dev@gmail.com>
2020-06-05 16:10:33 +0200

    Add components to structure the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,29 @@
 import React, { Fragment } from 'react';
 
 import Parallax from '../Parallax';
+import News from '../News';
+import About from '../About';
+import Members from '../Members';
+import Projects from '../Projects';
+import Membership from '../Membership';
+import Contact from '../Contact';
+import { GeneralMeetingsLink } from '../GeneralMeetingsLink';
 
 export default function Home() {
   return (
     <Fragment>
       <Parallax />
+      <div className="wrap">
+        <News />
+        <About />
+        <Members />
+        <hr />
+        <Projects />
+      </div>
+      <GeneralMeetingsLink />
+      <Membership />
+      <hr />
+      <Contact />
     </Fragment>
   );
 }
