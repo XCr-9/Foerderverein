c5873e1108990f497752949095906a9ef876604d
XCr-9 <t.t.first.dev@gmail.com>
2020-06-05 21:47:15 +0200

    Add some comments

--- src/components/pages/Statute.js
+++ src/components/pages/Statute.js
@@ -13,10 +13,12 @@ export default function Statute() {
         <section key={paragraph.heading}>
           <h2>{paragraph.heading}</h2>
 
+          {/* intro text is a text without numbering */}
           {paragraph.intro && paragraph.intro.text
             ? paragraph.intro.text
             : null}
 
+          {/* intro list is an unsorted list related to intro text */}
           {paragraph.intro && paragraph.intro.list ? (
             <ul style={{ marginTop: '0' }}>
               {paragraph.intro.list.map((listItem) => (
@@ -25,11 +27,14 @@ export default function Statute() {
             </ul>
           ) : null}
 
+          {/* text is an object that includes an ordered list (numbers) and possibly a further/nested list orderes by letters */}
           {paragraph.text ? (
             <ol>
               {paragraph.text.map((text) => (
                 <li key={text.number}>
                   {text.number}
+
+                  {/* letters is a nested list oredered by letters */}
                   {text.letters ? (
                     <ol className="letter">
                       {text.letters.map((letter) => (
@@ -44,6 +49,7 @@ export default function Statute() {
         </section>
       ))}
 
+      {/* PDF Download */}
       <p style={{ marginTop: '80px' }}>
         <b>{legal.statute.pdf.heading}</b>
         <a href={legal.statute.pdf.link[0]}>{legal.statute.pdf.link[1]}</a>
