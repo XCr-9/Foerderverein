0a5e27b57b3b1f47a673acdd74b5dd0a40a42f01
XCr-9 <t.t.first.dev@gmail.com>
2020-06-05 13:37:56 +0200

    Structure the contact page and move the component in another directory

---
+++ src/components/Contact.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useContext } from 'react';
+import styled from 'styled-components';
+
+import { Context } from '../Context';
+import { TwoColumns } from './Columns';
+import { Heading } from './Headings';
+
+export default function Contact() {
+  const { content } = useContext(Context);
+
+  return (
+    <StyledContainer>
+      <div className="wrap">
+        <Heading h2 title={content.contact.heading} id="kontakt" />
+        <TwoColumns columnLeft="0 0 48%" columnRight="0 0 48%">
+          <div className="column-left">
+            <Heading h3 title={content.contact.sections[0].heading} />
+            {content.contact.sections[0].content.map((line) => (
+              <Fragment key={line}>
+                {line}
+                <br />
+              </Fragment>
+            ))}
+          </div>
+          <div className="column-right">
+            <Heading h3 title={content.contact.sections[1].heading} />
+            {content.contact.sections[1].content.map((line) => (
+              <Fragment key={line}>
+                {line}
+                <br />
+              </Fragment>
+            ))}
+          </div>
+        </TwoColumns>
+      </div>
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.div`
+  background-color: #fff;
+  color: #000;
+  width: 100%;
+`;
