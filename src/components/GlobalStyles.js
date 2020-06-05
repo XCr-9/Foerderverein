ea8a3d39bb4c5e4276e40180a78b93775925fd65
XCr-9 <t.t.first.dev@gmail.com>
2020-06-05 21:29:33 +0200

    Add global styles for hr, ol and ul

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -20,11 +20,57 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
 
     .wrap {
-      width: 100%;
-      max-width: 1170px;
-      margin: 0 auto;
-      padding: 15px;
       box-sizing: border-box;
+      margin: 30px auto 60px auto;
+      max-width: 1170px;
+      padding: 15px;
+      width: 100%;
+
+      a {
+        color: #000;
+      }
+    }
+
+    ul {
+      padding-inline-start: 19px;
+
+      &.blank {
+        list-style-type: none;
+        padding: 0;
+      }
+    }
+
+    ol {
+      margin-top: 0;
+      padding-inline-start: 19px;
+
+      &.letter {
+        counter-reset: list;
+        padding-inline-start: 19px;
+
+        li {
+          list-style-type: none;
+          position: relative;
+
+          &:before {
+            counter-increment: list;
+            content: counter(list, lower-alpha) ") ";
+            left: -1.1em;
+            position: absolute;
+          }
+        }
+      }
+    }
+
+    hr {
+      background-color: #fff;
+      background-image: linear-gradient(to right, rgba(151, 27, 36, 0), rgba(151, 27, 36, 0.9), rgba(151, 27, 36, 0));
+      border: 0;
+      height: 1px;
+      margin: 80px auto;
+      max-width: 1170px;
+      padding: 0;
+      width: 100%;
     }
   }
 `;
