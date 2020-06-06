00bb29aa7feb72f46e675cac594f1e4774a55c90
XCr-9 <t.t.first.dev@gmail.com>
2020-06-06 23:05:31 +0200

    Add invitations page

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,18 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 
 import { ga } from '../helpers/analytics';
 
-const Home = lazy(() => import('./pages/Home'));
-const LegalNotice = lazy(() => import('./pages/LegalNotice'));
 const GeneralMeetings = lazy(() => import('./pages/GeneralMeetings'));
+const Home = lazy(() => import('./pages/Home'));
+const Invitation = lazy(() => import('./pages/Invitation'));
+const LegalNotice = lazy(() => import('./pages/LegalNotice'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy'));
 const Statute = lazy(() => import('./pages/Statute'));
-const NotFound = lazy(() => import('./pages/NotFound'));
 
 export default function Router() {
   return (
     <Switch>
       <Route exact path="/" component={ga(Home)} />
       <Route path="/datenschutzerklaerung" component={ga(PrivacyPolicy)} />
+      <Route path="/einladung" component={ga(Invitation)} />
       <Route path="/impressum" component={ga(LegalNotice)} />
       <Route path="/mitgliederversammlungen" component={ga(GeneralMeetings)} />
       <Route path="/satzung" component={ga(Statute)} />
---
+++ src/components/pages/Invitation.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useContext } from 'react';
+
+import { Context } from '../../Context';
+import { meta } from '../../helpers/meta';
+
+export default function Invitation() {
+  const { content } = useContext(Context);
+
+  document.title = content.invitation.meta.title;
+  meta('name', 'description', content.invitation.meta.description);
+
+  return (
+    <div className="wrap">
+      <h1>{content.invitation.heading}</h1>
+
+      {content.invitation.texts
+        ? content.invitation.texts.map((text) => (
+            <article key={text} dangerouslySetInnerHTML={{ __html: text }} />
+          ))
+        : content.invitation.default}
+    </div>
+  );
+}
