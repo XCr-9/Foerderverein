cfd645f7428ae584c3afcdceeb3f998d3ac7e231
XCr-9 <t.t.first.dev@gmail.com>
2020-06-06 23:20:25 +0200

    Beautify the jsx structure

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useInView } from 'react-intersection-observer';
 
@@ -28,17 +28,30 @@ export default function Membership() {
         }}
       >
         <Heading h2 title={content.membership.heading} id="mitglied-werden" />
+
         <TwoColumns columnLeft="0 0 48%" columnRight="0 0 48%">
           <div className="column-left">
             <Heading h3 title={content.membership.becomeMember.heading} />
-            {content.membership.becomeMember.text}
-            <a href={content.membership.becomeMember.link[0]}>
-              {content.membership.becomeMember.link[1]}
-            </a>
+            <p>
+              {content.membership.becomeMember.text}
+
+              <a href={content.membership.becomeMember.link[0]}>
+                {content.membership.becomeMember.link[1]}
+              </a>
+            </p>
           </div>
+
           <div className="column-right">
             <Heading h3 title={content.membership.donate.heading} />
             {content.membership.donate.text}
+            <p>
+              {content.contact.sections[1].content.map((line) => (
+                <Fragment key={line}>
+                  {line}
+                  <br />
+                </Fragment>
+              ))}
+            </p>
           </div>
         </TwoColumns>
       </div>
