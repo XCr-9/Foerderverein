f8988ab8e2b0837307a480d39e188888730509d2
XCr-9 <t.t.first.dev@gmail.com>
2020-06-06 13:40:03 +0200

    Add section for projects

---
+++ src/components/Projects.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+
+import { Context } from '../Context';
+import { Heading } from './Headings';
+import { TwoColumns } from './Columns';
+
+export default function Projects() {
+  const { content } = useContext(Context);
+
+  return (
+    <section>
+      <Heading h2 title={content.projects.heading} id="projekte" />
+      <TwoColumns columnLeft="80%" columnRight="20%">
+        <div className="column-left">
+          <ul>
+            {content.projects.list.map((listItem) => (
+              <li key={listItem}>{listItem}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="column-right">
+          <img
+            src={content.projects.img.src}
+            alt={content.projects.img.alt}
+            loading="lazy"
+            style={{ marginTop: '1em' }}
+          />
+        </div>
+      </TwoColumns>
+    </section>
+  );
+}
