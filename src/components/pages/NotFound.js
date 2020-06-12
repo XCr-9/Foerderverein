353f52cb0a43035aba16439bbfe0da25e5267594
XCr-9 <t.t.first.dev@gmail.com>
2020-06-12 16:38:29 +0200

    Add jsx structure for error 404 page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,9 @@
-import React, { Fragment, useContext } from 'react';
+import React, { useContext } from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 import { Context } from '../../Context';
 import { meta } from '../../helpers/meta';
+import { Heading } from '../Headings';
 
 export default function NotFound() {
   const { content } = useContext(Context);
@@ -10,8 +12,28 @@ export default function NotFound() {
   meta('name', 'description', content.notFound.meta.description);
 
   return (
-    <Fragment>
-      <h1>Error 404</h1>
-    </Fragment>
+    <div className="wrap">
+      <Heading h1 title={content.notFound.heading} />
+      {content.notFound.content.map((text) => (
+        <p key={text}>{text}</p>
+      ))}
+
+      <ul>
+        {content.header.navigation.map((navItem) => (
+          <li key={navItem.linkText}>
+            <HashLink style={{ textDecoration: 'none' }} to={navItem.href}>
+              {navItem.linkText}
+            </HashLink>
+          </li>
+        ))}
+        {content.footer.nav.map((navItem) => (
+          <li key={navItem.linkText}>
+            <HashLink style={{ textDecoration: 'none' }} to={navItem.href}>
+              {navItem.linkText}
+            </HashLink>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
