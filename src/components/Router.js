b114ba8bde92263045af1eeff115fe475426f8ef
XCr-9 <t.t.first.dev@gmail.com>
2020-07-23 17:39:46 +0200

    Load content via Context.js (useContext) instead of Content.js (import)

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,8 @@
-import React, { lazy } from 'react';
+import React, { lazy, useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { ga } from '../helpers/analytics';
-import { content } from '../data/Content';
+import { Context } from '../Context';
 
 const Blog = lazy(() => import('./pages/Blog'));
 const BlogPost = lazy(() => import('./pages/BlogPost'));
@@ -15,6 +15,8 @@ const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy'));
 const Statute = lazy(() => import('./pages/Statute'));
 
 export default function Router() {
+  const { content } = useContext(Context);
+
   return (
     <Switch>
       <Route exact path="/" component={ga(Home)} />
