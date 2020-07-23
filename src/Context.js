4c7adb7104eaf8901b0644860b22a307ff2f0af1
XCr-9 <t.t.first.dev@gmail.com>
2020-07-23 17:02:54 +0200

    Fetch content data from an api

--- src/Context.js
+++ src/Context.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 
-import { content } from './data/Content';
-import { legal } from './data/Legal';
+import { useFetch } from './helpers/useFetch';
+import Spinner from './components/Spinner';
 
 export const Context = React.createContext(null);
 
 export default function ContextProvider({ children }) {
   const [generalMeetingIndex, setGeneralMeetingIndex] = useState(0);
 
-  return (
-    <Context.Provider
-      value={{
-        content,
-        legal,
-        generalMeetingIndex,
-        setGeneralMeetingIndex,
-      }}
-    >
-      {children}
-    </Context.Provider>
-  );
+  const fetched = useFetch(`https://api.foerderverein-ggnauen.de`);
+
+  if (fetched && fetched.response) {
+    const content = fetched.response.data.content;
+    const legal = fetched.response.data.legal;
+
+    return (
+      <Context.Provider
+        value={{
+          content,
+          legal,
+          generalMeetingIndex,
+          setGeneralMeetingIndex,
+        }}
+      >
+        {children}
+      </Context.Provider>
+    );
+  } else if (fetched && fetched.error) {
+    return 'Es ist ein Fehler aufgetreten. Bitte besuchen Sie uns zu einem späteren Zeitpunkt erneut.';
+  }
+  return <Spinner />;
 }
